80421385bda07d89accc2ef36017a6a7cd276d46
Sourov Roy <sourovfci@gmail.com>
2017-12-26 22:42:06 +0600

    Payment completed using stripe checkout form

--- app/Http/Controllers/PurchasesController.php
+++ app/Http/Controllers/PurchasesController.php
@@ -12,6 +12,23 @@ class PurchasesController extends Controller
 	 */
     public function store(Request $request)
     {
-    	dd($request->all());
+        //dd($request->all());
+
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $customer = Customer::create(array(
+            'email' => $request->get('stripeEmail'),
+            'source'  => $request->get('stripeToken')
+        ));
+
+        $charge = Charge::create(array(
+            'customer' => $customer->id,
+            'amount'   => 2500,
+            'currency' => 'usd'
+        ));
+
+        var_dump($charge);
+        echo '<h1>Successfully charged $25.00!</h1>';
+
     }
 }
